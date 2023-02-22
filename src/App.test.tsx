f3436cb3c65cb282079d4867ad65ae8d16f4d553
Ryan <ryanlearyzafra@gmail.com>
2023-02-22 21:31:52 +0100

    Add app test for sonar bugfix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const a = 1;
+  const b = 1;
+  const r = a + b;
+  expect(r).toBe(2);
 });
